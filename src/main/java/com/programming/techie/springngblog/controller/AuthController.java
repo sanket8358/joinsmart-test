d30d7887d0d274e98f29fd6e64a3181dc765bd91
sanket8358 <sanketpatwardhan8358@gmail.com>
2019-12-02 01:25:14 +0530

    added api to get profile from linkedin bug fix

--- src/main/java/com/programming/techie/springngblog/controller/AuthController.java
+++ src/main/java/com/programming/techie/springngblog/controller/AuthController.java
@@ -12,8 +12,13 @@ import com.programming.techie.springngblog.service.AuthenticationResponse;
 import com.sun.net.httpserver.Headers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -28,26 +33,26 @@ public class AuthController {
 
 
     @PostMapping("/getuserprofile")
-    public String getUserProfileFromLinkedIn(@RequestBody String token) {
+    public String getUserProfileFromLinkedIn(@RequestBody Map<String,String> reqBody) {
+        String code = reqBody.get("token");
         HttpHeaders headers=headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setBearerAuth(token);
+        headers.setBearerAuth(code);
         String url="https://api.linkedin.com/v2/me";
         String forObject = restTemplate.getForObject(url, String.class);
         return forObject;
     }
-    
+
     @PostMapping("/getaccesstoken")
-    public String getAccessToken(@RequestBody String code) {
-        System.out.println(code);
+    public String getAccessToken(@RequestBody Map<String,String> reqBody) {
+        String code=reqBody.get("code");
         String linkedInAccessTokenUrl="https://www.linkedin.com/oauth/v2/accessToken";
         String body = "grant_type=authorization_code&code=" + code + "&redirect_uri=https://joinsmart.herokuapp.com/api/auth/callback&client_id=81sirvv927wpon&client_secret=t4OpCZ376aWqhOFe";
-
         HttpHeaders headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<String> request =
+        HttpEntity<String> payload =
                 new HttpEntity<String>(body, headers);
-        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(linkedInAccessTokenUrl, request, String.class);
+        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(linkedInAccessTokenUrl, payload, String.class);
         String res=stringResponseEntity.getBody();
         String access_token = new Gson().fromJson(res, JsonElement.class).getAsJsonObject().get("access_token").toString();
 
